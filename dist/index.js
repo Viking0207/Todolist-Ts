e9c1f39320aa376730c18130cde3c42cdf427244
Viking0207 <fickyfebryan0@gmail.com>
2024-07-26 13:30:59 +0700

    Kerangka awal Todo-List

---
+++ dist/index.js
@@ -0,0 +1,33 @@
+"use strict";
+//Pilih element
+const btnConfirm = document.querySelector(".todo-btn");
+const inputList = document.querySelector(".todo-input");
+const formTodo = document.querySelector(".todo-form");
+const todoList = document.querySelector(".todo-List");
+const btnDelete = document.querySelector(".todo-dltall");
+//Mengatur Tombol Submit
+const handleSubmit = (e) => {
+    e.preventDefault();
+    //Membuat objek baru
+    const newTodo = {
+        id: Date.now(),
+        todo: inputList.value,
+        completed: false
+    };
+    //Penyimpanan Todo ke local storage
+    //Menambahkan todo baru
+    const newLi = document.createElement('li');
+    const checkB = document.createElement('input');
+    checkB.type = "checkbox";
+    //Tambahkan checkbox EvLi
+    checkB.addEventListener('change', () => {
+        console.log("Checked");
+        newTodo.completed = checkB.checked;
+    });
+    newLi.append(newTodo.todo, checkB);
+    todoList.prepend(newLi);
+    //Reset Input
+    inputList.value = "";
+};
+//menambahkan form event listener
+formTodo.addEventListener('submit', e => handleSubmit(e));
